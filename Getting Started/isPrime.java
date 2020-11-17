9967c3b3ed94d5f6f2f97c7813f34822586ddc89
manan-bedi2908 <53931841+manan-bedi2908@users.noreply.github.com>
2020-11-17 19:50:23 +0530

    add

---
+++ Getting Started/isPrime.java
@@ -0,0 +1,25 @@
+import java.util.*;
+  
+  public class Main {
+      public static void main(String[] args) {
+          Scanner scn = new Scanner(System.in);
+          int t = scn.nextInt();
+          
+          for (int i = 0; i < t; i++) {
+              int n = scn.nextInt();
+              int div = 2;
+              
+              for (div = 2; div*div <= n; div++) {
+                  if(n % div == 0) {
+                      break;
+                  }
+              }
+              if (div * div > n) {
+                  System.out.println("prime");
+              }
+              else {
+                  System.out.println("not prime");
+              }
+          }
+      }
+  }
